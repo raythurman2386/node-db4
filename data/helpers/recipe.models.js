1f1d5eeebeb1f5fe8cef438013d67d550bf87aa4
raythurman2386 <raymondthurman5@gmail.com>
2020-01-08 22:13:50 -0500

    initial models file

---
+++ data/helpers/recipe.models.js
@@ -0,0 +1,13 @@
+const db = require('../db-config');
+
+function getRecipes() {}
+
+function getShoppingList(recipe_id) {}
+
+function getInstructions(recipe_id) {}
+
+module.exports = {
+  getRecipes,
+  getShoppingList,
+  getInstructions,
+};
