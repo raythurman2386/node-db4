322c394ef8cf3c13662b1b449897c2c697401a9d
raythurman2386 <raymondthurman5@gmail.com>
2020-01-08 22:07:59 -0500

    cleanup file complete

--- data/seeds/00-cleanup.js
+++ data/seeds/00-cleanup.js
@@ -1,9 +1,7 @@
-
-exports.seed = await function(knex) {
+exports.seed = async function(knex) {
   // Deletes ALL existing entries
-  await knex('recipe_ingredients').truncate()
-  await knex('ingredients').truncate()
-  await knex('steps').truncate()
-  await knex('recipes').truncate()
-
+  await knex('recipe_ingredients').truncate();
+  await knex('ingredients').truncate();
+  await knex('steps').truncate();
+  await knex('recipes').truncate();
 };
