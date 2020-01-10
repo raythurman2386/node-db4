ae0ac0a05982669be052e729fbb231b96fccde41
raythurman2386 <raymondthurman5@gmail.com>
2020-01-09 19:29:35 -0500

    recipe model works in db browser

--- data/helpers/recipe.models.js
+++ data/helpers/recipe.models.js
@@ -4,7 +4,12 @@ function getRecipes() {
   return db('recipes').select();
 }
 
-function getShoppingList(recipe_id) {}
+function getShoppingList(recipe_id) {
+  return db('recipe_ingredients as RI')
+    .join('ingredients as I', 'RI.ingredient_id', 'I.Id')
+    .select('I.name')
+    .where({ recipe_id: recipe_id });
+}
 
 function getInstructions(recipe_id) {}
 
