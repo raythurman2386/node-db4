9723315db53407caafcc98f0e77b1f60797a5f21
raythurman2386 <raymondthurman5@gmail.com>
2020-01-09 19:37:37 -0500

    starting STRETCH with routes

--- api/routes/recipe.routes.js
+++ api/routes/recipe.routes.js
@@ -14,6 +14,12 @@ recipeRouter
   // single recipe shopping list
   .get('/recipes/:id/shoppingList', async (req, res, next) => {
     // TODO
+    try {
+      const list = await Recipes.getShoppingList(req.params.id);
+      return res.status(200).json(recipes);
+    } catch (error) {
+      next(error);
+    }
   })
   // instructions for single recipe
   .get('/recipes/:id/instructions', async (req, res, next) => {
